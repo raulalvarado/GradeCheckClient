164485ef16d26f5f38491c1515272da7d8c8f464
ar4 <ooskarargueta08@gmail.com>
2018-10-09 17:40:35 -0600

    Ahora es posible agregar nuevas carreras

--- js/careers.js
+++ js/careers.js
@@ -4,6 +4,9 @@ var updateCarFrm = $("#frmUpdtCarrers");
 var updateModalCareer = $("#actualizarFacultad");
 var newModalCareer = $("#nuevaCarrera");
 var deleteModalFaculty = $("#eliminarCarrera");
+var faculties = $(".cCarrerFaculty");
+var career_type = $(".cCareerType");
+
 //trying to get faculties from digital ocean server
 $(document).ready(function() {
     getCareers();
@@ -44,6 +47,8 @@ function getCareers() {
                     "</tr>");
             });
             newCarFrm.trigger("reset");
+            getFaculties();
+            getCareerTypes();
         },
         error: function(error) {
             console.log(error);
@@ -52,4 +57,90 @@ function getCareers() {
             })
         }
     });
+}
+
+//ajax request to get faculties
+function getFaculties() {
+    $.ajax({
+        url: BASE_URL + FACULTIES_READ_ACTIVE,
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            console.log(result);
+            faculties.empty();
+            $.each(result, function(i, val) {
+                console.log(val.name);
+
+                //filling table
+                faculties.append("<option value=" + val.id + ">" + val.name + "</option>");
+            });
+            faculties.formSelect();
+        },
+        error: function(error) {
+            console.log(error);
+            M.toast({
+                html: 'Error al solicitar todas las facultades'
+            })
+        }
+    });
+}
+
+//ajax request to get faculties
+function getCareerTypes() {
+    $.ajax({
+        url: BASE_URL + CAREER_TYPES_READ_ACTIVE,
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            console.log(result);
+            career_type.empty();
+            $.each(result, function(i, val) {
+                console.log(val.name);
+                //filling table
+                career_type.append("<option value=" + val.id + ">" + val.name + "</option>");
+            });
+            career_type.formSelect();
+        },
+        error: function(error) {
+            console.log(error);
+            M.toast({
+                html: 'Error al solicitar todas las facultades'
+            })
+        }
+    });
+}
+
+//save career
+function postCareer() {
+    $.ajax({
+        url: BASE_URL + CAREERS_CREATE,
+        type: "POST",
+        data: newCarFrm.serialize(),
+        success: function(result) {
+            console.log(result);
+            getCareers();
+            newModalCareer.modal('close');
+            M.toast({
+                html: 'Carrera agregada con exito'
+            })
+        },
+        error: function(error) {
+            console.log(error.responseText);
+            showError(error.responseText);
+        }
+    });
+}
+
+//save career post
+newCarFrm.submit(function(e) {
+    e.preventDefault();
+    postCareer();
+});
+
+//display server errors
+function showError(error) {
+    M.toast({
+        html: error
+    })
+
 }
--- js/server.js
+++ js/server.js
@@ -6,14 +6,17 @@ var STUDENTS_UPDATE = "Students/update"
 var STUDENTS_DELETE = "Students/delete/"
 
 var FACULTIES_READ = "faculties"
+var FACULTIES_READ_ACTIVE = "faculties/active"
 var FACULTIES_CREATE = "faculties"
 var FACULTIES_GET = "Faculties/readById/"
 var FACULTIES_UPDATE = "Faculties/update"
 var FACULTIES_DELETE = "Faculties/delete/"
 
 var CAREERS_READ = "careers/full"
+var CAREERS_CREATE = "careers"
 
 var CAREER_TYPES_READ = "careerTypes"
+var CAREER_TYPES_READ_ACTIVE = "careerTypes/active";
 var CAREER_TYPES_CREATE = "careerTypes"
 
 var COURSES_READ = "courses/faculties/prerrequisite"
